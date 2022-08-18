cf55999f9f279f272640f0bba64aa6bfafded9da
Marlonnnz <marlonnunez1695@gmail.com>
2022-08-18 10:01:47 -0500

    Create ChatService.cs

---
+++ dotnet/Services/ChatService.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.AspNetCore.SignalR;
+using Sabio.Models.Domain.Messages;
+using Sabio.Models.Interfaces;
+using System.Threading.Tasks;
+
+namespace Sabio.Web.Api.Models.Messages
+{
+    public class ChatHub : Hub<IChatClient>
+    {
+        public async Task SendMessage(Conversation message)
+        {
+            await Clients.All.ReceiveMessage(message);
+        }
+    }
+}
