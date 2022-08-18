4c08d4ddca1a239e9c64f4d7ab46f8514b6350e5
Marlonnnz <marlonnunez1695@gmail.com>
2022-08-18 09:52:17 -0500

    Add files via upload

---
+++ dotnet/Models/Domain/Conversation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.Messages
+{
+    public class Conversation
+    {    
+        public UserProfile SenderProfile { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime DateSent { get; set; }
+        public DateTime DateRead { get; set; }
+        public UserProfile RecipientProfile { get; set; }
+    }
+}
